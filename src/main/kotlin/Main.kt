5ab49d118d21d47fd79694656797bbf7b57b72bd
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-03 10:26:26 +0300

    Employee salary calculation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,20 @@
-package org.example
+import java.util.Scanner
+
+class Employee(
+    var name: String,
+    var baseSalary: Int
+) {
+    fun calculateBonus() {
+        val reader = Scanner(System.`in`)
+        println("Enter number of years")
+        val years = reader.nextInt()
+        val bonus = 0.05 * (baseSalary * years)
+        val totalSalary = baseSalary + bonus
+        println("$name's total salary is $totalSalary")
+    }
+}
 
 fun main() {
-    println("Hello World!")
+    val employee = Employee("Frank", 120000)
+    employee.calculateBonus()
 }
